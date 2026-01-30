d750ecc726efef3d600d7ee234996673018ca82d
Evan Arnott <evanarnott17@gmail.com>
2026-01-29 21:36:40 -0700

    Added background and static image

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using SimpleAnimationNamespace;
 
 namespace Assignment_01;
 
@@ -8,6 +9,14 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+
+    //creating variables for sprites
+    private Texture2D _background;
+
+    private Texture2D _staticImage;
+
+    //game position variables
+    private Vector2 _wagonPosition = new Vector2(350,225);
 
     public Game1()
     {
@@ -19,6 +28,10 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
+        //Setting game window size
+        _graphics.PreferredBackBufferWidth = 576;
+        _graphics.PreferredBackBufferHeight = 320;
+        _graphics.ApplyChanges();
 
         base.Initialize();
     }
@@ -28,6 +41,8 @@ public class Game1 : Game
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         // TODO: use this.Content to load your game content here
+        _background = Content.Load<Texture2D>("Background");
+        _staticImage = Content.Load<Texture2D>("Chest");
     }
 
     protected override void Update(GameTime gameTime)
@@ -42,9 +57,19 @@ public class Game1 : Game
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Color.Gold);
 
         // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+
+        //Drawing the background image
+        _spriteBatch.Draw(_background, Vector2.Zero, Color.White);
+
+        //Drawing the static image
+        _spriteBatch.Draw(_staticImage, _wagonPosition, Color.White);
+
+        _spriteBatch.End();
+
 
         base.Draw(gameTime);
     }
